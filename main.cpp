a86efc871fb581d607e9b6e1b286288817d6af2e
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-06-02 19:01:22 -0400

    Limit fps to 60.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 using std::cout;
 using std::endl;
+using sclock = std::chrono::steady_clock;
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "draw.h"
@@ -12,7 +13,12 @@ int main() {
 	std::thread audioThread(audioThreadMain, &audio);
 	if (!initialize_gl()) { cout << "graphics borked. exiting." << endl; return 0; };
 	glfwSetTime(0.0);
-	while (should_loop()) { draw(&audio); }
+	while (should_loop()) {
+		// limit fps to 60
+		auto start = sclock::now();
+		draw(&audio);
+		std::this_thread::sleep_for(sclock::duration(std::chrono::milliseconds(16)) - (sclock::now() - start));
+	}
 	audio.thread_join = true;
 	audioThread.join();
 	deinit_drawing();
