75d9d38946c45775113ab3f3f0983d4e1359c6f1
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-06-06 10:51:07 -0400

    Rename variables and check for negative durations in frame limiting code.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <thread>
 using std::cout;
 using std::endl;
-using sclock = std::chrono::steady_clock;
+#include <chrono>
+using clk = std::chrono::steady_clock;
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "draw.h"
@@ -14,10 +15,10 @@ int main() {
 	if (!initialize_gl()) { cout << "graphics borked. exiting." << endl; return 0; };
 	glfwSetTime(0.0);
 	while (should_loop()) {
-		// limit fps to 60
-		auto start = sclock::now();
+		auto start = clk::now();
 		draw(&audio);
-		std::this_thread::sleep_for(sclock::duration(std::chrono::milliseconds(16)) - (sclock::now() - start));
+		auto dura = clk::duration(std::chrono::milliseconds(16)) - (clk::now() - start);
+		std::this_thread::sleep_for(dura > clk::duration(0) ? dura : clk::duration(0));
 	}
 	audio.thread_join = true;
 	audioThread.join();
