c863d83a2354b20b11813cd2c3f7a71f953645d8
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-12-05 10:48:45 -0500

    Update main.cpp to be compatible with the changes made to audio_process.h.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using clk = std::chrono::steady_clock;
 #include <GLFW/glfw3.h>
 #include "draw.h"
 #include "audio_data.h"
+#include "audio_process.h"
 
 #ifdef WINDOWS
 #ifdef DEBUG
@@ -18,18 +19,21 @@ int main() {
 int main() {
 #endif
 
-	struct audio_data audio;
-	audio.thread_join = false;
-	std::thread audioThread(audioThreadMain, &audio);
+	struct audio_data my_audio_data;
+	my_audio_data.thread_join = false;
+
+	audio_processor* ap = new audio_processor(&my_audio_data, &get_pcm, &audio_setup);
+	std::thread audioThread(&audio_processor::run, ap);
+
 	if (!initialize_gl()) { cout << "graphics borked. exiting." << endl; return 0; };
 	glfwSetTime(0.0);
 	while (should_loop()) {
 		auto start = clk::now();
-		draw(&audio);
+		draw(&my_audio_data);
 		auto dura = clk::duration(std::chrono::milliseconds(16)) - (clk::now() - start);
 		std::this_thread::sleep_for(dura > clk::duration(0) ? dura : clk::duration(0));
 	}
-	audio.thread_join = true;
+	my_audio_data.thread_join = true;
 	audioThread.join();
 	deinit_drawing();
 	glfwTerminate();
