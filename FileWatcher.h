692c91c2928a7bbc082fbbe17bf92febd4379548
Bradley Bauer <gazingatstars.o7@gmail.com>
2018-01-07 13:10:46 -0500

    Rearrange filewatcher and add a sleep statement.

--- FileWatcher.h
+++ FileWatcher.h
@@ -20,25 +20,25 @@ public:
 	}
 
 	// More than one event can be delivered by the editor from a single save command.
-	// So process the event and set the last process time.
-	// If new event is within 200 ms of last process time, then ignore it.
-	//
+	// So sleep a few millis and then process the event and set the last process time.
+	// If new event is within 100 ms of last process time, then ignore it.
 	// If shader.json or any frag or geom file has changed, then set shaders_changed.
 	void handleFileAction(FW::WatchID watchid, const FW::String& dir, const FW::String& filename_str, FW::Action action)
 	{
 		if (FW::Action::Delete == action)
 			return;
+		if (dir != "shaders")
+			return;
+		std::string extension = filesys::path(filename_str).extension().string();
+		if (extension != ".json" && extension != ".geom" && extension != ".frag")
+			return;
+		if (std::chrono::steady_clock::now() - last_event_time < std::chrono::milliseconds(100))
+			return;
 
-		filesys::path filename = filesys::path(filename_str);
-		std::string extension = filename.extension().string();
-		std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
-		float time_elapsed = (now - last_event_time).count() / 1e6;
-		if (time_elapsed > 200.f)
-			if (dir == "shaders")
-				if (extension == ".json" || extension == ".geom" || extension == ".frag") {
-					shaders_changed = true;
-					last_event_time = now;
-				}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+		shaders_changed = true;
+		last_event_time = std::chrono::steady_clock::now();
 	}
 	bool shaders_changed;
 
